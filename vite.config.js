4af878af325755c651bba2623702aeff460f30f1
Samuel <escobar.samuel@correounivalle.edu.co>
2026-04-20 16:58:52 -0500

    feat: Avances Entregable 2 - Definición de Voz, Tono y Microcopy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: { //Para que funcione ngrok
-    allowedHosts: [
-      'adopted-quentin-frowsty.ngrok-free.dev'
-    ]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    server: { // Para que funcione ngrok
+      allowedHosts: [
+        env.VITE_ALLOWED_HOST
+      ]
+    }
   }
 })
+
